9619cfedab33718ea9cdac399efc731bc03d1f22
ben <benoit.works@gmail.com>
2017-09-26 16:42:45 +0200

    create a twig extension to display properly boolean values

---
+++ src/AppBundle/Twig/AppExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Twig;
+
+
+use Symfony\Component\Translation\TranslatorInterface;
+
+class AppExtension extends \Twig_Extension
+{
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('boolean', array($this, 'booleanFilter')),
+        );
+    }
+
+    public function booleanFilter($value)
+    {
+        if ($value) {
+            return $this->translator->trans('yes', [], 'common');
+        } else {
+            return $this->translator->trans('no', [], 'common');
+        }
+    }
+
+    public function getName()
+    {
+        return 'app_extension';
+    }
+}
