867028ee3c092aad82b5b4dce40577cd02bbf722
ben <benoit.works@gmail.com>
2018-01-18 12:15:13 +0100

    LTI register consumer command

---
+++ src/AppBundle/Command/LTIRegisterConsumer.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use IMSGlobal\LTI\ToolProvider;
+use IMSGlobal\LTI\ToolProvider\DataConnector;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
+
+
+class LTIRegisterConsumer extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:lti-register-consumer')
+            ->setDescription('register a new lti consumer tool.')
+            ->setHelp('This command registers a new lti consumer tool')
+            ->addOption(
+                'consumerKey',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'What is the consumer key (eg: \'testing.edu\')?'
+            )
+            ->addOption(
+                'consumerName',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'What is the consumer name (eg: \Testing\')?'
+            )
+            ->addOption(
+                'consumerSecret',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'What is the consumer secret (eg: \'ThisIsASecret!\')?'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'Register a lti consumer tool',
+            '============',
+            '',
+        ]);
+
+        if (!$consumerKey = $input->getOption('consumerKey')) {
+            throw new MissingMandatoryParametersException('consumerKey');
+        }
+        if (!$consumerName = $input->getOption('consumerName')) {
+            throw new MissingMandatoryParametersException('consumerName');
+        }
+        if (!$consumerSecret = $input->getOption('consumerSecret')) {
+            throw new MissingMandatoryParametersException('consumerSecret');
+        }
+        $output->writeln([
+            'You are about to register a new LTI consumer with the following parameters:',
+            'consumerKey: ' . $consumerKey,
+            'consumerName: ' . $consumerName,
+            'consumerSecret: ' . $consumerSecret,
+        ]);
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion('It that ok (y/n)?', false);
+        $output->writeln('');
+
+        if (!$helper->ask($input, $output, $question)) {
+            return;
+        }
+        $dsn = sprintf('mysql:host=%s;dbname=%s', getenv('DB_HOST'), getenv('DB_NAME'));
+        $db = new \PDO($dsn, getenv('DB_USER'), getenv('DB_PWD'));
+        $dataConnector = DataConnector\DataConnector::getDataConnector('', $db);
+        $consumer = new ToolProvider\ToolConsumer($consumerKey, $dataConnector);
+        $consumer->name = $consumerName;
+        $consumer->secret = $consumerSecret;
+        $consumer->enabled = TRUE;
+        $consumer->save();
+        $output->writeln('Your consumer app has been created!');
+
+    }
+}
