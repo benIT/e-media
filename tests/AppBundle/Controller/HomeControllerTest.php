1ffa3fb092be0c6a7409ba64585e1c3635613370
ben <benoit.works@gmail.com>
2018-01-19 10:52:31 +0100

    update phpunit test

--- tests/AppBundle/Controller/HomeControllerTest.php
+++ tests/AppBundle/Controller/HomeControllerTest.php
@@ -2,15 +2,27 @@
 
 namespace Tests\AppBundle\Controller;
 
+use AppBundle\DataFixtures\ORM\Data\UserFixturesData;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
+    private $authInfo;
+
+    protected function setUp()
+    {
+        $testUser = UserFixturesData::$data[0]['firstName'];
+        $this->authInfo = [
+            'PHP_AUTH_USER' => $testUser,
+            'PHP_AUTH_PW' => $testUser,
+        ];
+    }
     public function testIndex()
     {
         (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__.'/../../../.env');
 
         $client = static::createClient();
+        $client->setServerParameters($this->authInfo);
         $route = $client->getKernel()->getContainer()->get('router')->generate('home_index');
         $crawler = $client->request('GET', $route);
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
