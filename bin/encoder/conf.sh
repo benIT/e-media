d8402a5b4cb486496be0b7c5db03cbf08582bdc3
ben <benoit.works@gmail.com>
2018-02-07 09:53:41 +0100

    relative path in encoder

--- bin/encoder/conf.sh
+++ bin/encoder/conf.sh
@@ -2,6 +2,6 @@
 ##################################################################
 #configuration
 ##################################################################
-MONITORDIR="/vagrant/shared/e-media/web/e-media-data/video"
+MONITORDIR="web/e-media-data/video"
 LOGFILE='var/logs/encoder.log'
 declare -a VIDEO_EXTENSION=("mp4" "flv" "mkv");
